7fcc3af1f68ef49c9055f3400594ea5f04872f5e
Pekes317 <El_Dante565@yahoo.com>
2019-08-09 19:47:01 -0500

    Add fixes  to handle nest types
    Fixed issue with reading from definition property

--- index.js
+++ index.js
@@ -1,85 +1,112 @@
-const fs = require('fs');
-const json = require('json-pointer');
+const fs = require("fs");
+const json = require("json-pointer");
 
 module.exports = generate;
 
+let rootSchema = {};
+
 function generate(schema, options = {}) {
-  let jsdoc = '';
+  let jsdoc = "";
 
   if (!schema || Object.keys(schema).length === 0) {
     return jsdoc;
   }
 
-  jsdoc += '/**\n';
+  rootSchema = { ...schema };
+  jsdoc += "/**\n";
   jsdoc += writeDescription(schema);
 
-  if (!json.has(schema, '/properties')){
-   return jsdoc;
+  if (!json.has(schema, "/properties")) {
+    return jsdoc;
   }
 
   jsdoc += processProperties(schema, false, options);
 
-  jsdoc += '  */\n';
+  jsdoc += "  */\n";
 
   return jsdoc;
 }
 
 function processProperties(schema, nested, options = {}) {
-  const props = json.get(schema, '/properties');
-  const required = json.has(schema, '/required') ? json.get(schema, '/required') : [];
+  const objDefs = [];
+  const props = json.get(schema, "/properties");
+  const required = json.has(schema, "/required")
+    ? json.get(schema, "/required")
+    : [];
 
-  let text = '';
+  let text = "";
   for (let property in props) {
+    const propName = upperFirst(property);
     if (Array.isArray(options.ignore) && options.ignore.includes(property)) {
       continue;
     } else {
-      let prefix = nested ? '.' : '';
-
-      if (props[property].type === 'object' && props[property].properties) {
-        text += writeParam('object', prefix + property, props[property].description, true);
-        text += processProperties(props[property], true);
-      } else {
-        let optional = !required.includes(property);
-        let type = getType(props[property]) || upperFirst(property);
-        text += writeParam(type, prefix + property, props[property].description, optional);
+      let prefix = nested ? "." : "";
+      let optional = !required.includes(property);
+      let prop = getProp(props[property], propName) || upperFirst(property);
+      let objType = prop.type === "object" && prop.properties;
+      let type = objType ? upperFirst(property) : prop.type;
+      text += writeParam(type, prefix + property, prop.description, optional);
+      if (objType) {
+        objDefs.push(writeTypeDef(prop, property, props.description));
       }
-    } 
+    }
   }
+  if (objDefs.length > 0) {
+    objDefs.forEach(objDef => {
+      text += objDef;
+    });
+  }
+
   return text;
 }
 
-function writeDescription(schema, suffix = 'object') {
+function writeDescription(schema, suffix = "object") {
   let text = schema.description || `Represents a ${schema.id} ${suffix}`;
+  text += schema.title ? `\n * @description ${schema.title}` : "";
   text += `\n  * @name ${upperFirst(schema.id)}`;
   return `  * ${text}\n  *\n`;
 }
 
-function writeParam(type = '', field, description = '', optional) {
+function writeParam(type = "", field, description = "", optional) {
   const fieldTemplate = optional ? `[${field}]` : field;
   return `  * @property {${type}} ${fieldTemplate} - ${description} \n`;
 }
 
-function getType(schema) {
+function writeTypeDef(obj = { type: "" }, field, description = "") {
+  const fieldTemplate = upperFirst(field);
+  return ` *\n  * @typedef {${
+    obj.type
+  }} ${fieldTemplate} - ${description} \n *\n ${processProperties(obj, false)}`;
+}
+
+function getProp(schema, propName = "") {
+  if (schema.type === "object" && !schema.description) {
+    schema.description = `Represents a ${propName} object`;
+  }
+
   if (schema.$ref) {
-    const ref = json.get(root, schema.$ref.substr(1));
-    return getType(ref);
+    const ref = json.get(rootSchema, schema.$ref.substr(1));
+    return getProp(ref);
   }
 
   if (schema.enum) {
-    return 'enum';
-  } 
+    schema.type = "enum";
+    return schema;
+  }
 
   if (Array.isArray(schema.type)) {
-    if (schema.type.includes('null')) {
-      return `?${schema.type[0]}`;
+    if (schema.type.includes("null")) {
+      schema.type = `?${schema.type[0]}`;
+      return schema;
     } else {
-      return schema.type.join('|');
+      schema.type = schema.type.join("|");
+      return schema;
     }
   }
 
-  return schema.type;
+  return schema;
 }
 
-function upperFirst(str = '') {
-  return str.substr(0,1).toUpperCase() + str.substr(1);
+function upperFirst(str = "") {
+  return str.substr(0, 1).toUpperCase() + str.substr(1);
 }
